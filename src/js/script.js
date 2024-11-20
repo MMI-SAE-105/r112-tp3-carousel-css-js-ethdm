6dbf25c449866b8d558600db27f75f6a3692b5a2
ethdm <ethan.hennequin--de_meyer@edu.univ-fcomte.fr>
2024-11-20 17:27:18 +0100

    carrousel fonctionne

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
 // *** CAROUSEL ***
 // TODO
 
+
+const carousel = document.querySelector(".carousel__container");
+carousel.scrollLeft = 520;
+
+carousel.scrollBy({ left: +260, behavior: "smooth" });
+
+carousel.scrollBy({ left: -260, behavior: "smooth" });
